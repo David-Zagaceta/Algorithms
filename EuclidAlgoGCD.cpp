5c1cb09f9c8b9a9828b11b4c9f80aadf4eceb74f
Dean <sayred1@unlv.nevada.edu>
2020-06-22 10:43:34 -0700

    add comments

--- EuclidAlgoGCD.cpp
+++ EuclidAlgoGCD.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 
-// Of two natural number a & b, this euclidean algo. can calculate their GCD, g. g is the largest number that divides into a & b w/o leavin a remainder. g = gcd(a,b).
+// Of two natural number a & b, euclid_algo calculates their GCD, g. g is the largest number that divides into a & b w/o leavin a remainder. g = gcd(a,b).
+// In order for this to work, gcd(a,b) >= gcd(b, r). where r = a%b.
+
 void euclid_algo(int, int);
 int euclid_algo_r(int, int);
 
@@ -15,18 +17,24 @@ int main(){
 };
 
 void euclid_algo(int a, int b){
+	
+	// swap a and b if conditions met
 	if (a < b){
 		int tmp = a;
 		a = b;
 		b = tmp;
 	}
+
+	// calculate and print GCD(a,b)
 	std::cout << "GCD(" << a <<","<< b << ")=" << euclid_algo_r(a, b) << std::endl;
 
 }
 
 int euclid_algo_r(int a, int b){
-	if (a%b == 0){return b;}
-	else{
+
+	// recursive implementation of the euclidean algorithm
+	if (a%b == 0){return b;}		
+	else{				
 		return euclid_algo_r(b, a%b);
 	}
 }
